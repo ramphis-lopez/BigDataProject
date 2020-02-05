64606f4eb30f65672ea65fe375f063dc88fabed8
Andrea Miranda <andrea.miranda1@upr.edu>
2020-02-05 13:12:09 -0400

    Dictionaries

--- Mainclass.py
+++ Mainclass.py
@@ -1,15 +1,39 @@
 #BigData Project
 #Contributos: Abiesel, Ramphis, Alexander, Laizla & Andrea
 
-import panda as pd
-import numpy as np 
+#import panda as pd
+#import numpy as np 
 
 #input code
 
 
 #Text Analysis
+"""
+    Disruption Type: 
+        hurricane related:
+            power - 1
+            communication - 2
+            water - 3
+            wastewater - 4
+            transportation - 5
+            other - 6
+    
+        hurricante not related: 7
 
+    Disruption Status:
+        actual disruption - 1
+        not disruption - 0
+        
+        
 
+"""
+tweetlistHR = {hurricane:[], water; [], power: [], communication: [], wastewater: [], transportion:[], other: []}
+
+tweetlistHR[hurricane] = ["hurricane Irma", "hurricane"]
+tweetlistHR[water] = ["bottle of water", "drinking water", "water", "bottled water"]
+tweetlistHR[power] = ["power off", "power down", "lost power"; "fallen power service"; 
+                "faller power cables", "fallen power spot"; "power back", "power is back",
+                ]
 
 
 
