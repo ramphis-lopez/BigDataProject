8d078e4e4ba9560cb11a682ee614f73e6f841da5
Andrea Miranda <andrea.miranda1@upr.edu>
2020-02-14 23:19:11 -0400

    Input ready

--- Mainclass.py
+++ Mainclass.py
@@ -1,28 +1,29 @@
 #BigData Project
 #Contributos: Abiesel, Ramphis, Alexander, Laizla & Andrea
 
+#imports
 import csv
 
 
 #input code
-'''
-filename = input('Enter the file name: ')
-filehandle = open(filename, 'r')
-'''
+file_tweets = []
+file_name = input("Enter the file name: ")
 
+default_file_name = 'ASCE_Samples_v2.csv' 
+if(len(file_name)<= 1):
+    file_name = default_file_name
 
-results = []
-with open('ASCE_Samples_v2.csv') as File:
+with open(file_name) as File:
     reader = csv.DictReader(File)
     for row in reader:
-        results.append(row)
+        file_tweets.append(row)
 '''
     for loop saves every sentences
 '''
-sentences = []
+sentences = [] #Contains every sentences of each row
 counter = 0
-for i in results:
-    sentences.append(results[counter]['ï»¿text'])
+for i in file_tweets:
+    sentences.append(file_tweets[counter]['ï»¿text'])
     counter+= 1
 
 
@@ -31,12 +32,7 @@ for i in results:
 """
     Disruption Type: 
         hurricane related:
-            power - 1
-            communication - 2
-            water - 3
-            wastewater - 4
-            transportation - 5
-            other - 6
+            power: 1, communication: 2, water: 3, wastewater: 4, transportation : 5,  other -:6
     
         hurricante not related: 7
 
@@ -54,13 +50,35 @@ tweetlistHR['power'] = ["power off", "power down", "lost power", "fallen power s
                 "fallen power cables", "fallen power spot", "power back", "power is back",
                 ]
 '''
-def evaluate_
+def evaluate_text(s, l):
+    rslt = []
+    founded = False
+    for word in s.split():
+        for i in l:
+            if(word == i):
+                founded = True
+                return 
+    
+    return True
+
+def assign_number(s):
+    if s == 'power':
+        return 1
+    elif s == 'communication':
+        return 2
+    elif s == 'water':
+        return 3
+    elif s == 'wastewater':
+        return 4
+    elif s == 'transportation':
+        return 5
+    elif s == 'other':
+        return 6
+    else:
+        return 7
 
 
 
-
-        
-#print(textAnalysis(txt))
 
 
 #output code
